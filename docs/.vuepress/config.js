d56da11b68b6c8648c1fff68c96dce3b740bac90
Yattoz <12408848+yattoz@users.noreply.github.com>
2020-11-25 13:00:37 +0100

    added Yuu theme, dark theme with red accent by default

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,22 @@
 module.exports = {
+	  theme: 'yuu',
 	  title: 'Calvinball Consortium',
 	  description: 'Le Calvinball Consortium est un réseau de podcasts rigolos.',
 	  head: [
-		['link', { rel: 'icon', href: '/favicon.png' }]
+		['link', { rel: 'icon', href: 'favicon.png' }],
+		// ['link', { rel: 'stylesheet', href: 'custom.css'}], // must be loaded after the theme
 	  ],
 	  themeConfig: {
 		  //test: 'podcasts/calvinball'
-		  search: true
+		  search: true,
+		  yuu:  {
+			  defaultDarkTheme: true,
+			  defaultColorTheme: 'red',
+			  colorThemes: ['red'],
+			  disableThemeIgnore: true,
+			  extraOptions: {
+				  // after: 'ToggleDarkMode',
+			  },
+		  },
 	  }
 }
--- docs/README.md
+++ docs/README.md
@@ -15,6 +15,8 @@ features:
   details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
 - title: Performant
   details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
-footer: MIT Licensed | Copyright © 2020 Yattoz
+footer: MIT Licensed | Copyright © 2020 Yattoz | Using theme vuepress-theme-yuu
 
 ---
+<link rel="stylesheet" href="custom.css">
+<!-- <ToggleDarkMode/> -->
