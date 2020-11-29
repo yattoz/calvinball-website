7756a424ec9a07b4e3351bb5642de661e2f54ac0
Yattoz <12408848+yattoz@users.noreply.github.com>
2020-11-29 12:49:04 +0100

    added People component, can render Markdown inside Front Matter

---
+++ docs/.vuepress/components/People.vue
@@ -0,0 +1,44 @@
+<!-- .vuepress/theme/layouts/GlobalLayout.vue -->
+<template>
+  <div class="people">
+    <h1>
+      {{this.$frontmatter.name}}
+    </h1>
+    <img :src="this.$frontmatter.image"/>
+    <div v-html="description_html">
+    </div>
+  </div>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it';
+
+export default {
+  computed: {
+    description_html() {
+      let md_options = {
+                          html: true,
+                          breaks: true,
+                          linkify: true,
+                          typographer: true
+                        }
+      let md = new MarkdownIt(md_options);
+      let res = md.render(this.$frontmatter.description)
+      console.log(res)
+      return res;
+      // return markdown.toHTML( this.$frontmatter.description );
+    }
+  }
+}
+</script>
+<style scoped>
+  img {
+    border-radius: 99em;
+    width: auto;
+    max-height: 200px;
+  }
+
+  .people {
+    text-align: center;
+  }
+</style>
---
+++ docs/people/yattoz.md
@@ -0,0 +1,10 @@
+---
+name: Yattoz
+podcasts:
+    - recommande
+image: /people/yattoz.jpg
+description: "Mélomane, nerd, toujours à la recherche de sons cheulous."
+
+---
+
+<People/>
