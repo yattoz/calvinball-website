83d304a826392bc0ea45708454b60a7c4778dabf
Yattoz <12408848+yattoz@users.noreply.github.com>
2020-12-29 10:39:44 +0100

    added deps for Debian (different from Ubuntu)

--- bootstrap_root.sh
+++ bootstrap_root.sh
@@ -9,30 +9,30 @@ if [[ $USER != "root" ]]; then
 	exit 1
 fi
 
-# build Nokogiri
-apt-get install -y build-essential git patch zlib1g-dev liblzma-dev
-# build rmagick
-apt-get install -y libmagickwand-dev
 # web server and certbot
 apt-get install -y nginx-full
 apt-get install -y python3-certbot-nginx
+apt-get install -y build-essential git curl autoconf bison
 
-# dependencies for rbenv to build ruby according to https://linuxize.com/post/how-to-install-ruby-on-ubuntu-20-04/
-apt-get install -y git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
-
-# Ubuntu 20.04 has Ruby 2.7, Debian 10 has Ruby 2.5 which is still perfectly viable.
-# apt-get install -y ruby ruby-dev 
+# build Nokogiri
+apt-get install -y patch zlib1g-dev liblzma-dev
+# build rmagick
+apt-get install -y libmagickwand-dev
 
 string=$(uname -a)
 if [[ $string == *"Debian"* ]]; then
 	curl -sL https://deb.nodesource.com/setup_lts.x | bash -
 	apt-get install -y nodejs
+	# dependencies for rbenv to build ruby on Debian https://linuxize.com/post/how-to-install-ruby-on-debian-10/
+	apt-get install -y libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
 fi
 
 # Using Ubuntu
 if [[ $string == *"Ubuntu"* ]]; then
 	curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 	sudo apt-get install -y nodejs
+	# dependencies for rbenv to build ruby on Ubuntu according to https://linuxize.com/post/how-to-install-ruby-on-ubuntu-20-04/
+	apt-get install -y libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 fi
 
 
