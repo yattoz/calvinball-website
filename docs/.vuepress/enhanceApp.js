82e1f6446af5173e37964de449c252bee5e397db
Yattoz <12408848+yattoz@users.noreply.github.com>
2021-01-09 23:08:52 +0100

    woops forgot to rename MJEE

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -13,14 +13,14 @@ export default ({
     "leretourdujeudi": {
         theme: {
           "tokens": {
-            "brand": "#9115E6FF",
-            "brandDark": "#582373FF",
-            "brandDarkest": "#3C1A4AFF",
-            "brandLightest": "#CFAAE5FF",
-            "shadeDark": "#807E7C",
+            "brand": "#000000FF",
+            "brandDark": "#808080FF",
+            "brandDarkest": "#000000FF",
+            "brandLightest": "#FEF65BFF",
+            "shadeDark": "#4A90E2FF",
             "shadeBase": "#807E7C",
-            "contrast": "#000",
-            "alt": "#fff"
+            "contrast": "#000000FF",
+            "alt": "#000000FF"
           }
         }
       },
@@ -38,17 +38,143 @@ export default ({
           }
         }
       },
-      "recommande": {
+      "calweebball": {
         theme: {
           "tokens": {
-            "brand": "#0D2FA3FF",
-            "brandDark": "#234173FF",
-            "brandDarkest": "#1A274AFF",
-            "brandLightest": "#B4D2F3FF",
+            "brand": "#80FFE6FF",
+            "brandDark": "#FCFFFCFF",
+            "brandDarkest": "#8497A1FF",
+            "brandLightest": "#4050C5FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#FFFEFEFF",
+            "alt": "#40B9C5FF"
+          }
+        }
+      },
+      "capycast": {
+        theme: {
+          "tokens": {
+            "brand": "#FFF20CFF",
+            "brandDark": "#FFF20CFF",
+            "brandDarkest": "#8497A1FF",
+            "brandLightest": "#D96800FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#FFF20CFF",
+            "alt": "#AD3400FF"
+          }
+        }
+      },
+      "lebestiairedesbesties": {
+        theme: {
+          "tokens": {
+            "brand": "#000000FF",
+            "brandDark": "#000000FF",
+            "brandDarkest": "#000000FF",
+            "brandLightest": "#FFB200FF",
             "shadeDark": "#807E7C",
             "shadeBase": "#807E7C",
             "contrast": "#000",
             "alt": "#fff"
+          }
+        }
+      },
+      "lebestiairedesbesties": {
+        theme: {
+          "tokens": {
+            "brand": "#000000FF",
+            "brandDark": "#000000FF",
+            "brandDarkest": "#000000FF",
+            "brandLightest": "#FFB200FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#000",
+            "alt": "#fff"
+          }
+        }
+      },
+      "ksdd": {
+        theme: {
+          "tokens": {
+            "brand": "#FF1A1AFF",
+            "brandDark": "#FFCA1AFF",
+            "brandDarkest": "#FFCA1AFF",
+            "brandLightest": "#666666FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#FFCA1AFF",
+            "alt": "#343434FF"
+          }
+        }
+      },
+      "lappeldekathulu": {
+        theme: {
+          "tokens": {
+            "brand": "#4A4A4AFF",
+            "brandDark": "#5F3808FF",
+            "brandDarkest": "#1A3A4A",
+            "brandLightest": "#EFCEA6FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#000",
+            "alt": "#fff"
+          }
+        }
+      },
+      "mjee": {
+        theme: {
+          "tokens": {
+            "brand": "#E61515FF",
+            "brandDark": "#3E321AFF",
+            "brandDarkest": "#3E321AFF",
+            "brandLightest": "#FBD685FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#3E321AFF",
+            "alt": "#fff"
+          }
+        }
+      },
+      "lesreglesdujeu": {
+        theme: {
+          "tokens": {
+            "brand": "#EA0909FF",
+            "brandDark": "#1F4499FF",
+            "brandDarkest": "#1A3A4A",
+            "brandLightest": "#AEF0F3FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#000",
+            "alt": "#fff"
+          }
+        }
+      },
+      "ludographie": {
+        theme: {
+          "tokens": {
+            "brand": "#F6EF77FF",
+            "brandDark": "#FFF600FF",
+            "brandDarkest": "#1A3A4A",
+            "brandLightest": "#7A2810FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#FFFFFFFF",
+            "alt": "#000000FF"
+          }
+        }
+      },
+      "recommande": {
+        theme: {
+          "tokens": {
+            "brand": "#CDCDCDFF",
+            "brandDark": "#94CFFBFF",
+            "brandDarkest": "#8CBBDEFF",
+            "brandLightest": "#2B4254FF",
+            "shadeDark": "#807E7C",
+            "shadeBase": "#807E7C",
+            "contrast": "#FFFFFFFF",
+            "alt": "#010101FF"
           }
         }
       }
