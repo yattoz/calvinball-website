26db31b66ea7eb418c42c1a297fd5c2b1774ce8e
Yattoz <12408848+yattoz@users.noreply.github.com>
2022-05-26 16:07:15 +0900

    avoid white blink when dark mode, avoid toggle blink

--- assets/js/toggle-dark.js
+++ assets/js/toggle-dark.js
@@ -1,6 +1,8 @@
 function toggleDarkTheme() {
     const body = document.body;
     body.classList.toggle("dark-mode");
+    document.documentElement.classList.toggle("dark-mode");
+
     //If dark mode is selected
     if (body.classList.contains("dark-mode")) {
       //Save user preference in storage
@@ -8,20 +10,29 @@ function toggleDarkTheme() {
       //If light mode is selected
     } else {
       body.classList.remove("dark-mode");
+      document.documentElement.classList.remove("dark-mode");
       setTimeout(function () {
         localStorage.removeItem("dark-theme");
       }, 100);
     }
 }
 
-document.getElementById("theme-toggle").addEventListener('change', () => {
-    console.log("change");
-    toggleDarkTheme();
-})
+
+
 
 document.addEventListener( "DOMContentLoaded", () => {
+    let checked = false
     if (localStorage.getItem("dark-theme")) {
         document.body.classList.add("dark-mode");
-        document.getElementById("theme-toggle").checked = true;
-      }
+        document.documentElement.classList.add("dark-mode");
+        checked = true;
+    }
+    let toggle_html = `    <input type="checkbox" id="theme-toggle" ${checked? "checked" : ""}/>
+    <label for="theme-toggle"><span></span></label>`
+    document.getElementById("dark-mode-widget").innerHTML = toggle_html;
+    
+    document.getElementById("theme-toggle").addEventListener('change', () => {
+      console.log("change");
+      toggleDarkTheme();
+    })
 })
