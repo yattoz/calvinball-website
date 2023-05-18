4c199bc9d06a3778ecd02de7d472b9f0ac82725a
Yattoz <12408848+yattoz@users.noreply.github.com>
2023-05-18 08:42:05 +0200

    typo, should run OK now, using bundle

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-PATH="$PATH:$HOME/.rbenv/bin:$HOME/.rbenv/shims"
+# PATH="$PATH:$HOME/.rbenv/bin:$HOME/.rbenv/shims"
 cd /home/yattoz/calvinball-website 
 echo "start from schedule" > start.log
 git pull 2>&1 > git.log
-ruby scripts/podcast_resources.rb --rebuild 2>&1 > podcast_resources.log
-ruby scripts/xmpp_ring --message="Podcast published from scheduled task"
+bundle exec ruby scripts/podcast_resources.rb --rebuild 2>&1 > podcast_resources.log
+bundle exec ruby scripts/xmpp_ring.rb --message="Podcast published from scheduled task"
